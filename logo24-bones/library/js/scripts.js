b7cca7879448e6971744e66da3d59931d90e7fbb
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-10-22 14:23:25 +0200

    Add dropdown menu

--- logo24-bones/library/js/scripts.js
+++ logo24-bones/library/js/scripts.js
@@ -12,34 +12,4 @@ slow the page load.
 // as the page loads, call these scripts
 jQuery(document).ready(function($) {
 
-  // Fade in and out sub menus
-  $("#menu-main > li > a").click(function(){
-    submenu = $(this).closest('li').find("ul:first");
-
-    // if submenu not present, returning true preserves the click so link will work
-    if(submenu.length == 0)
-      return true;
-
-    submenu.fadeToggle(100, "linear");
-
-    // returning false, cancels the click, hence not jumping to the top
-    return false;
-  })
-
-  // Close menu when on click away
-  $("body").click(function(event) {
-   if (!$(event.target).closest("#menu-main > li").length) {
-       $(".sub-menu").fadeOut(100, "linear");
-   };
-  });
-  
-  // Toggle navigation visibility on small screens and the toggle icon.
-  // And rotate toggle icon
-  $(".open-navigation").click(function () {
-    $('#menu-main').toggleClass('menu-toggle');
-    $(".icon-reorder").toggleClass('rotate');
-    $(".wrapper-navigation").toggleClass('open');
-      return false;
-  });
-
 }); /* end of as page load scripts */
