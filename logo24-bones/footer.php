e396fd9eae3c202ea475ff89f5321869b9313f01
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-10-24 21:44:44 +0200

    Mock in booking form

--- logo24-bones/footer.php
+++ logo24-bones/footer.php
@@ -6,6 +6,7 @@
       <div class="copyright"><p>&copy;CELT 2012. Registered Charity CHY 14519</p>
         <p>To sponsor projects, programmes and events <a href="/contact-us/">Contact Us.</a></p>
         <p>Sponsors benefit from media coverage and advertising in our marketing material.</p>
+        <p>CELT, Tuamgraney, Scariff, Co.Clare. Phone: <span class="tel">+353 640 765</span>. Email: <a href="mailto:info@celtnet.org">info@celtnet.org</a></p>
 
       </div>
     </footer>
--- logo24-bones/template-courses-index.php
+++ logo24-bones/template-courses-index.php
@@ -9,44 +9,13 @@ get_header(); ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
       <section class="m-all t1 d1" role="complementary">
-        <?php wp_nav_menu(array('menu'=>'Courses')); ?>
+        <?php wp_nav_menu(array('menu'=>'courses')); ?>
       </section>
 
       <section role="main" class="m-all t2 d2-d4">
         <header>
           <h1><?php the_title(); ?></h1>
         </header>
-        
-        <h2 id="agro-courses">Agroforestry Introductory Courses</h2>
-        
-        <ul class="course-index-list">
-          <li>
-            <div class="date">Saturday 7th July 2012:</div>
-            <div class="title">Creating a forest garden Creating a forest garden Creating a forest garden</div>
-            <a href="/creating-a-forest-garden">Book Now</a>
-          </li>
-          <li>
-            <div class="date">Saturday 7th July 2012</div>
-            <div class="title">Creating a forest garden</div>
-            <a href="/creating-a-forest-garden">Book Now</a>
-          </li>
-        </ul>
-        
-        <h2 id="imecofarm-courses">CELT Courses at Imecofarm</h2>
-        
-        <ul class="course-index-list">
-          <li>
-            <div class="date">Saturday 7th July 2012</div>
-            <div class="title">Creating a forest garden</div>
-            <a href="/creating-a-forest-garden">Book Now</a>
-          </li>
-          <li>
-            <div class="date">Saturday 7th July 2012</div>
-            <div class="title">Creating a forest garden</div>
-            <a href="/creating-a-forest-garden">Book Now</a>
-          </li>
-        </ul>
-        
         <?php the_content(); ?>
       </section> <!-- end article section -->
 
