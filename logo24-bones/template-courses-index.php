700532e9536c658aa4df29f084d66b3b9b38180f
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-10-24 19:06:58 +0200

    Add Courses template

---
+++ logo24-bones/template-courses-index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Courses index
+*/
+get_header(); ?>
+  
+  <div class="page">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+      <section class="m-all t1 d1" role="complementary">
+        <?php wp_nav_menu(array('menu'=>'Courses')); ?>
+      </section>
+
+      <section role="main" class="m-all t2 d2-d4">
+        <header>
+          <h1><?php the_title(); ?></h1>
+        </header>
+        <?php the_content(); ?>
+      </section> <!-- end article section -->
+
+    <?php endwhile; else: ?>
+      <p>Whatever you were trying, it didn't work...</p>
+    <?php endif; ?>
+  
+  </div> <?php #page ?>
+
+<?php get_footer(); ?>
