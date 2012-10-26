55d67f854aab75aa7a0661e46869c516f59495f1
Patrick O'Hanlon <paddy@paddyohanlon.com>
2012-10-26 23:33:43 +0200

    Style home page list.

--- logo24-bones/library/bones.php
+++ logo24-bones/library/bones.php
@@ -387,7 +387,7 @@ function bones_filter_ptags_on_images($content){
 function bones_excerpt_more($more) {
 	global $post;
 	// edit here if you like
-	return '...  <a href="'. get_permalink($post->ID) . '" title="Read '.get_the_title($post->ID).'">Read more &raquo;</a>';
+	return '...  <a href="'. get_permalink($post->ID) . '" title="Read '.get_the_title($post->ID).'">Read more</a>';
 }
 
 
